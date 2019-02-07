79751814db1ed6a99d4e8d28213edfb7eab2cba8
milindjon <raghuvarma330@gmail.com>
2019-02-07 15:15:12 +0000

    initial commit

---
+++ CTL/anykernel.sh
@@ -0,0 +1,34 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=KernelTest
+do.devicecheck=
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=CTL
+device.name2=CTL_sprout
+device.name3=
+device.name4=
+device.name5=
+'; } # end properties
+
+# shell variables
+block=auto;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel install
+dump_boot;
+write_boot;
+
+## end install
+
---
+++ build_CTL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+echo "--------------------------------------------"
+echo "Welcome To Kernel Source"
+echo "Lets Compile kernel sit Back Have A coffee "
+echo "--------------------------------------------"
+
+
+cd
+git clone https://github.com/milindjon/aarch64-linux-android-4.9.git aarch64-linux-android-4.9
+cd kernel
+rm -r output
+export ARCH=arm64
+export CROSS_COMPILE=/home/ubuntu/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+clear
+mkdir output
+make -C $(pwd) O=output CTL_defconfig
+make -j32 -C $(pwd) O=output
+cp -r output/arch/arm64/boot/Image.gz-dtb /home/ubuntu/kernel/CTL
+cd CTL
+zip -r CTL_Sprout_9.0_RV-3.0-KERNEL-By-Raghuvarma.zip META-INF patch tools Image.gz-dtb anykernel.sh
+gdrive upload CTL_Sprout_9.0_RV-3.0-KERNEL-By-Raghuvarma.zip
+cd ..
+
+
+echo "--------------------------------------------"
+echo "All Right now flash at your own risk"
+echo "Flashable Zip located in RV Folder"
+echo "Good Bye ..!! @ RaghuVarma "
+echo "--------------------------------------------"
